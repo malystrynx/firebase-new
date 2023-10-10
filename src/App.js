a6246936f52e5978996540da1ffc186ec3b27a1d
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2023-10-10 12:31:41 +1100

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './App.css';
-import { auth } from './firebase/init';
-import { createUserWithEmailAndPassword,
-signInWithEmailAndPassword, signOut, onAuthStateChanged
+import React from "react";
+import "./App.css";
+import { auth } from "./firebase/init";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 
-
 function App() {
+  const [user, setUser] = React.useState({});
+
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       console.log(user);
@@ -15,35 +46,9 @@ function App() {
       }
     });
   }, []);
-  function register(){
-    console.log('register');
-    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
-    .then((user)=>{
-      console.log(user)
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-  }
-
-  function login(){
-    signInWithEmailAndPassword (auth, 'email@email.com', 'test123')
-    .then(({user})=>{
-      console.log(user)
-      setUser(user)
-    })
-    .catch((error)=>{
-      console.log(error.message)
-    })
-  }
-
-  function logout(){
-    signOut(auth);
-    setUser({})
-  }
 
   return (
-    <div className="App">
+    <div>
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
